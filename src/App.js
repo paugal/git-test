3ae3a7e5187055a2d40191a4639b3cff0378b7eb
paugal <50529290+paugal@users.noreply.github.com>
2022-07-21 11:29:02 +0200

    add Body iin App

--- src/App.js
+++ src/App.js
@@ -18,6 +18,12 @@ function App() {
           Learn React   BRANCH TEST
         </a>
       </header>
+      <body>
+        <h1>Href Attribute Example</h1>
+        <p>
+          <a href="https://www.freecodecamp.org/contribute/">The freeCodeCamp Contribution Page</a> shows you how and where you can contribute to freeCodeCamp's community and growth.
+        </p>
+      </body>
     </div>
   );
 }
